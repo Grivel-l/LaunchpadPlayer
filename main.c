31524073edfd57ab8e57d0cba7729f4180245c88
Grivel-l <legrivel@student.le-101.fr>
2019-06-10 15:07:47 +0200

    Added devices support

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <libusb-1.0/libusb.h>
 
@@ -9,7 +11,9 @@ static libusb_device  *getPad(libusb_device **list) {
   i = 0;
   while (list[i]) {
     libusb_get_device_descriptor(list[i], &desc);
-    if (desc.idVendor == 0x1235 && desc.idProduct == 0x000e) {
+    if ((desc.idVendor == 0x1235 && desc.idProduct == 0x000e) ||
+        (desc.idVendor == 0x09e8 && desc.idProduct == 0x0076) ||
+        (desc.idVendor == 0x09e8 && desc.idProduct == 0x0075)) {
       dprintf(2, "Found launchpad\n");
       return (list[i]);
     }
@@ -21,6 +25,7 @@ static libusb_device  *getPad(libusb_device **list) {
 static int  handle_pad(libusb_device *pad) {
   int             error;
   unsigned char   data[150];
+  char        c[200];
   libusb_device_handle  *handle;
 
   if (libusb_open(pad, &handle) != 0)
@@ -38,8 +43,14 @@ static int  handle_pad(libusb_device *pad) {
     return (-1);
   }
   while (1) {
-    if ((error = libusb_bulk_transfer(handle, 0x81, data, 150, NULL, 0)) != 0)
+    memset(c, 0, 200);
+    if ((error = libusb_interrupt_transfer(handle, 0x81, data, 150, NULL, 0)) != 0)
       dprintf(2, "Couldn't receive info %s\n", libusb_strerror(error));
+    strcat(c, "mplayer sounds/");
+    c[strlen(c)] = data[0] + 48;
+    strcat(c, ".wav");
+    if (data[1] != 0) 
+      system(c);
     dprintf(2, "Data: %i, %i, %i, %i, %i, %i\n", data[0],data[1], data[2],data[3],data[4],data[5]);
   }
   libusb_release_interface(handle, 0);
@@ -60,6 +71,7 @@ int   main(void) {
     return (1);
   }
   if ((pad = getPad(list)) == NULL) {
+    dprintf(2, "Couldn't find any device\n");
     libusb_free_device_list(list, 1);
     libusb_exit(NULL);
     return (1);
